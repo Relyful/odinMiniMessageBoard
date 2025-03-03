29b5c3393fa13573c2a210c1b04ba7bdc9cd834e
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-03 20:56:53 +0100

    Setup Express server, basic EJS index

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const indexRouter = require("./routes/indexRouter");
+
+const PORT = 8080;
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Welcome to your server, listening on ${PORT}.`);  
+});
