bda4df769cdb05f2be4c8c630b0353aa1d87f89a
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-04 20:57:41 +0100

    Add post route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
+const { format } = require('date-fns')
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: format(new Date(), "yyyy-MM-dd")
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: format(new Date(), "yyyy-MM-dd")
   }
 ];
 
@@ -19,5 +20,11 @@ const indexRouter = Router();
 indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages});
 });
+indexRouter.get('/new', (req, res) => {
+  res.render('form');
+});
+indexRouter.post('/new', (req, res) => {
+  
+})
 
 module.exports = indexRouter;
