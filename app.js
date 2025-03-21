8c004a07867bac03f9e3e70bfd8e66288762cae1
Relyful <13381270+Relyful@users.noreply.github.com>
2025-03-21 21:41:11 +0100

    Install pg, dotenv, setup script to populate new db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const indexRouter = require("./routes/indexRouter");
+require('dotenv').config();
 
 const PORT = 8080;
 const app = express();
---
+++ db/populatedb.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+require('dotenv').config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR (255),
+  text VARCHAR (255),
+  added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO messages (username, text, added) 
+VALUES
+  ('Relyful', 'First', '2025-03-05 14:30:00'),
+  ('hiq', 'hi', '2025-03-05 14:30:00'),
+  ('CB', 'Good job!', '2025-03-06 14:30:00'),
+  ('MD Munna Hasan Leon', '741348', '2025-03-06 14:30:00'),
+  ('h', 'mr robot', '2025-03-07 14:30:00');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.CONNECTION_STRING,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
